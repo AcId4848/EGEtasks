e6ea87820a4f147557cbca175472df9a8c6d85b8
AcId4848 <142092198+AcId4848@users.noreply.github.com>
2025-05-12 22:15:35 +0700

    12.05.2025

---
+++ KOMPEGE/block 12/block 12 task 624.py
@@ -0,0 +1,7 @@
+for a1 in range(100):
+    for b1 in range(100):
+        for c1 in range(100):
+            if 4 + 4 * (2 + a1) == 24 and 8 + 4 * (-4 + b1) == 16 and 10 + 4 * (-5 + c1) == 10:
+                print(a1, b1, c1, sep="")
+
+# 365
---
+++ KOMPEGE/block 12/block 12 task 656.py
@@ -0,0 +1,6 @@
+for b in range(100):
+    for N in range(100):
+        if -3 + N * 10 + 2 == 59 and -5 + N * (b - 12) + 3 == 46:
+            print(N, b)
+
+# 20
